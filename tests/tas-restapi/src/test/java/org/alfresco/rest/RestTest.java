eea6920fa6f91416c864fa7007ee7f775f78f7ab
tiagos <tiagosalvado10@gmail.com>
2020-07-17 00:20:03 +0100

    added validations for testing purposes

--- tests/tas-restapi/src/test/java/org/alfresco/rest/RestTest.java
+++ tests/tas-restapi/src/test/java/org/alfresco/rest/RestTest.java
@@ -3,6 +3,7 @@ package org.alfresco.rest;
 import java.lang.reflect.Method;
 
 import org.alfresco.dataprep.WorkflowService;
+import org.alfresco.dataprep.WorkflowService.WorkflowType;
 import org.alfresco.rest.core.RestProperties;
 import org.alfresco.rest.core.RestWrapper;
 import org.alfresco.utility.LogFactory;
@@ -13,6 +14,7 @@ import org.alfresco.utility.data.DataLink;
 import org.alfresco.utility.data.DataSite;
 import org.alfresco.utility.data.DataUserAIS;
 import org.alfresco.utility.data.DataWorkflow;
+import org.alfresco.utility.exception.ServerReachableAlfrescoIsNotRunningException;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.network.ServerHealth;
 import org.slf4j.Logger;
@@ -63,45 +65,34 @@ public abstract class RestTest extends AbstractTestNGSpringContextTests
 
     protected SiteModel testSite;
 
-    @BeforeSuite(alwaysRun = true)
-    public void checkServerHealth() throws Exception
-    {
-		//super.springTestContextPrepareTestInstance();
-		serverHealth.assertServerIsOnline();
-        testSite = dataSite.createPublicRandomSite();
-    }
-    
-    /*
+   
     @BeforeSuite(alwaysRun = true)
     public void checkServerHealth() throws Exception
     {
     	try {
-    		checkServer();
+    		super.springTestContextPrepareTestInstance();
     	} catch (Exception e) {
-    		LOG.error("*** checkServerHealth attempt 1 error: " + e.getMessage());
-    		
-    		try {
-    			Thread.sleep(10000);
-    		} catch (Exception e2) {
-    			// do nothing
-    		}
-    		
-    		try {
-    			checkServer();
-			} catch (Exception e3) {
-				LOG.error("*** checkServerHealth attempt 2 error: " + e.getMessage());
-				throw e3;
-			}
+    		LOG.error("*** springTestContextPrepareTestInstance error: " + e.getMessage(), e);
     	}
+    	
+    	try {
+    		serverHealth.assertServerIsOnline();
+    	} catch (ServerReachableAlfrescoIsNotRunningException e) {
+    		Thread.sleep(15000);
+    		serverHealth.assertServerIsOnline();
+    	}
+		
+		if (WorkflowType.NewTask.getId() == null) {
+			if (workflow != null) {
+				workflow.afterPropertiesSet();
+			} else {
+				// not likely to happen
+				LOG.error("*** workflowService IS NULL");
+			}
+		}
+    	
         testSite = dataSite.createPublicRandomSite();
     }
-    
-    private void checkServer() throws Exception {
-		super.springTestContextPrepareTestInstance();
-		serverHealth.assertServerIsOnline();
-		workflow = applicationContext.getBean(WorkflowService.class);
-    }
-    */
     
     @BeforeMethod(alwaysRun=true)
     public void showStartTestInfo(Method method)
